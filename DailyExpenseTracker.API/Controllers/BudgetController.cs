0159807e9e0cad3cdb10568781f0082bd923a6b1
Amanhussain <112181574+Xoderaman@users.noreply.github.com>
2025-02-05 14:50:35 +0530

    Add files via upload

---
+++ DailyExpenseTracker.API/Controllers/BudgetController.cs
@@ -0,0 +1,29 @@
+﻿//using Microsoft.AspNetCore.Mvc;
+//using System.Threading.Tasks;
+
+//[Route("api/[controller]")]
+//[ApiController]
+//public class BudgetController : ControllerBase
+//{
+//    private readonly IBudgetService _budgetService;
+
+//    public BudgetController(IBudgetService budgetService)
+//    {
+//        _budgetService = budgetService;
+//    }
+   
+//    [HttpGet("GetBudgetNews")]
+//    public async Task<IActionResult> GetBudgetNews()
+//    {
+//        try
+//        {
+//            var newsResponse = await _budgetService.GetBudgetNewsAsync();
+//            return Ok(newsResponse); // Return JSON data
+//        }
+//        catch (Exception ex)
+//        {
+//            return BadRequest(new { message = ex.Message });
+//        }
+      
+//    }
+//}
